46507f45473663ca4219d29515a603e7a0a1d2b5
Artyom <38377534+ksydex@users.noreply.github.com>
2023-09-11 00:45:53 +0300

    1st subtask done

---
+++ lab1/src/Main.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите размер массива: ");
+        int size = scanner.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Введите элементы массива:");
+
+        for (int i = 0; i < size; i++) {
+            System.out.print("Элемент " + (i + 1) + ": ");
+            array[i] = scanner.nextInt();
+        }
+
+        scanner.close();
+
+        int sum = 0;
+
+        for (int i = 0; i < size; i++) {
+            sum += array[i];
+        }
+
+        double average = (double) sum / size;
+
+        System.out.println("Сумма элементов массива: " + sum);
+        System.out.println("Среднее арифметическое элементов массива: " + average);
+    }
+}
